78ce65fea8c050e0932a4287bfbc7a670dd5d2ea
Alexander Dziendziol <a.dziendziol@jarvia.de>
2018-10-26 01:13:27 +0200

    Editable Building with dynamic "Building Type" Added

--- src/app/building-detail/building-detail.component.ts
+++ src/app/building-detail/building-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Building} from '../classes/building';
+import { Building } from '../classes/building';
 import { BuildingService } from '../building/services/building.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -8,27 +8,42 @@ import { Location } from '@angular/common';
   selector: 'app-building-detail',
   templateUrl: './building-detail.component.html',
   styleUrls: ['./building-detail.component.css',
-  '../app.component.css']
+    '../app.component.css']
 })
 export class BuildingDetailComponent implements OnInit {
 
   building: Building;
+  selectedBuildingType: String = "Ressource";
+  buildingTypes = []; 
   
-  constructor( 
+ 
+
+  constructor(
     private buildingService: BuildingService,
     private activatedRoute: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
-    // this.getBuilding()
+    this.getBuilding();
+    this.getBuildingTypes();
   }
 
-/*   getBuilding(): void {
+
+
+
+  getBuilding(): void {
     const id = +this.activatedRoute.snapshot.paramMap.get('id');
     this.buildingService.getBuilding(id).subscribe(building => this.building = building);
-  } */
+  }
 
-  goBack(): void{
+  getBuildingTypes(): void{
+    this.buildingService.getBuildingTypes().subscribe(buildingtypes => this.buildingTypes = buildingtypes);
+  }
+
+  goBack(): void {
     this.location.back();
+  }
+  save(): void {
+    this.buildingService.updateBuilding(this.building).subscribe(() => this.goBack);
   }
 }
--- src/app/building/services/building.service.ts
+++ src/app/building/services/building.service.ts
@@ -1,37 +1,74 @@
 import { Injectable } from '@angular/core';
-import {Building} from '../../classes/building';
-import {Observable, of} from 'rxjs';
-import {MessageService} from '../../messages/services/message.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Building } from '../../classes/building';
+import { Observable, of } from 'rxjs';
+import { MessageService } from '../../messages/services/message.service';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuildingService {
   private buildingsUrl = "http://localhost:8080/api/building"; //ToDo -> Configfile
+ 
 
   constructor(
     private messageService: MessageService,
-    private http: HttpClient){ }
+    private http: HttpClient) { }
 
-  getBuildings(): Observable<Building[]>{
+  getBuildings(): Observable<Building[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('Buildingservice: Fetched Heroes')
-    return this.http.get<Building[]>(this.buildingsUrl)
-      .pipe(catchError(this.handleError('getHeroes', []))
+    this.messageService.add('Buildingservice: Fetched Buildings')
+    return this.http.get<Building[]>(this.buildingsUrl).pipe(
+      tap(_ => this.log('Fetched Buildings')),
+      catchError(this.handleError('getBuildings', []))
+      );
+  }
+
+
+  getBuilding(id: number): Observable<Building> {
+    const url = `${this.buildingsUrl}/${id}`;
+    return this.http.get<Building>(url).pipe(
+      tap(_ => this.log(`fetched Building id=${id}`)),
+      catchError(this.handleError<Building>(`getHero id=${id}`))
     );
   }
 
-/*   getBuilding(id: number): Observable<Building> {
-    this.messageService.add(`Buildingservice: Fetched Hero with ID:${id}`)
-    return of(Buildings.find(building => building.id === id));
-  } */
+  updateBuilding(building: Building): Observable<any>{
+    this.log(building.toString());
+    return this.http.put<Building>(this.buildingsUrl,building,httpOptions).pipe(
+      tap(_ => this.log(`Updated Building: ${building.id}`)),
+      catchError(this.handleError<any>('updateBuilding'))
+    );
+  }
 
-  private log(message: string){
+  getBuildingTypes(): Observable<String[]>{
+    const url = `${this.buildingsUrl}/buildingtypes`;
+    return this.http.get<String[]>(url).pipe(
+      tap(_ => this.log('Fetched BuildingsTypes')),
+      catchError(this.handleError('getBuildingTypes', []))
+      );
+  }
+
+
+
+
+
+
+
+
+  private log(message: string) {
     this.messageService.add(`BuildingService: ${message}`);
   }
+
+
+
+
+
 
   /**
  * Handle Http operation that failed.
@@ -39,18 +76,18 @@ export class BuildingService {
  * @param operation - name of the operation that failed
  * @param result - optional value to return as the observable result
  */
-private handleError<T> (operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
- 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
- 
-    // TODO: better job of transforming error for user consumption
-    this.log(`${operation} failed: ${error.message}`);
- 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 
 }
